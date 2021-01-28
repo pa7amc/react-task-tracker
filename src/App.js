aa0dd430f49b4f01701d065dbdaedefaad8e06f8
Patrícia Correia <pa7amc@gmail.com>
2021-01-28 15:22:38 +0000

    +task stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+        const [tasks, setTasks] = useState([
+                {
+                      id: 1,
+                      text: "Doc",
+                      day: "15th Feb at 2:30pm",
+                      reminder: true,
+                },
+                {
+                      id: 2,
+                      text: "Meeting",
+                      day: "4th Feb at 6:40pm",
+                      reminder: true,
+                },
+                {
+                      id: 3,
+                      text: "Shopping",
+                      day: "9th Feb at 11:00am",
+                      reminder: false,
+                },
+          ]);
       /*   const name = "Brad";
         const x = false; */
 
       return (
             <div className="cont1">
                   <Header />
-                  <Tasks />
+                  <Tasks tasks={tasks}/>
                   {/* 
       <h2>Hello {name}</h2>
       <h3>Hello {x ? "Y" : "N"}</h3> */}
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,13 @@
-import { useState } from "react";
+import Task from './Task'
 
-const Tasks = () => {
-        //tasks are now part of the State of component
-        //State is immutable
-      const [tasks, setTasks] = useState([
-            {
-                  id: 1,
-                  text: "Doc",
-                  day: "15th Feb at 2:30pm",
-                  reminder: true,
-            },
-            {
-                  id: 2,
-                  text: "Meeting",
-                  day: "4th Feb at 6:40pm",
-                  reminder: true,
-            },
-            {
-                  id: 3,
-                  text: "Shopping",
-                  day: "9th Feb at 11:00am",
-                  reminder: false,
-            },
-      ]);
+const Tasks = ({tasks}) => {
+    
+      
       //https://www.alura.com.br/artigos/javascript-quando-devo-usar-foreach-e-map
       return (
             <>
                   {tasks.map((task) => (
-                        <h3>{task.text}</h3>
+                        <Task key={task.id} task={task}/>
                   ))}
             </>
       );
