a508fdc2d862da278cfe885ba4c2539a51d60af0
Patrícia Correia <pa7amc@gmail.com>
2021-01-28 22:58:48 +0000

    addtask

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
       const [tasks, setTasks] = useState([
@@ -27,6 +28,13 @@ function App() {
       /*   const name = "Brad";
         const x = false; */
 
+      //Add task
+      const addTask = (task) => {
+            const id = Math.floor(Math.random() * 10000) + 1;
+            const newTask = { id, ...task };
+            setTasks([...tasks, newTask]);
+      };
+
       //Delete task
       const deleteTask = (id) => {
             setTasks(tasks.filter((task) => task.id !== id));
@@ -44,8 +52,9 @@ function App() {
       };
 
       return (
-            <div className="cont1">
+            <div className="container">
                   <Header />
+                  <AddTask onAdd={addTask} />
                   {tasks.length > 0 ? (
                         <Tasks
                               tasks={tasks}
