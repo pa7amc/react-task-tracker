d8ff466de31d380238accf470cf56ec82d766337
Patrícia Correia <pa7amc@gmail.com>
2021-01-29 02:11:18 +0000

    Brad Traversy name correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
                         Redone by <a href="https://pa7amc.github.io">pa7amc</a>.
                         Credits go to{" "}
                         <a href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA">
-                              Trasversy Media
+                              Brad Traversy
                         </a>
                   </p>
             </footer>
